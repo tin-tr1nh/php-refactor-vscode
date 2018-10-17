3410d754ca992b0f498c1c23b7e4d042d11e6a1e
Tin Trinh <trinh.tin@hamee.co.jp>
2018-10-17 17:54:17 +0900

    add walk function to traverse through AST tree, obtain potential params

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,80 @@ import * as vscode from "vscode";
 const PhpParser = require('php-parser');
 const UnParser = require('php-unparser');
 
+interface State {
+    vars: string[];
+    level: number;
+}
+
+function walk(node: any, state: State, params: string[]) {
+    console.log(`Walk to node ${node.kind}-${node.name}, can see vars ${state.vars}`);
+
+    // foreach
+    // will create new scope, state of inner scope mustn't change
+    // the state of outer scope, so create new state object
+    if (node.kind === "foreach") {
+
+        var newState: State = {
+            vars: state.vars.slice(),
+            level: state.level
+        };
+        newState.vars.push(node.value.name);
+        state = newState;
+
+        if (node.value !== undefined && node.value.kind === "variable") {
+            state.vars.push(node.value.name);
+        }
+    }
+
+    // check if current node is variable kind
+    // and not in current state, it could be the potential parameters to refactor
+    if (node.kind === "variable" && state.vars.indexOf(node.name) === -1) {
+        params.push(node.name);
+    }
+
+    // after check walk inside if possible 
+    // through children (block), arguments, left, right, arguments (call), what,
+    // body (foreach), source (foreach)
+
+    if (node.children !== undefined && node.children instanceof Array) {
+        node.children.forEach((child: any) => {
+            walk(child, state, params);
+        });
+    }
+
+    if (node.arguments !== undefined && node.arguments instanceof Array) {
+        node.arguments.forEach((child: any) => {
+            walk(child, state, params);
+        });
+    }
+
+    if (node.left !== undefined) {
+        walk(node.left, state, params);
+    }
+
+    if (node.right !== undefined) {
+        walk(node.right, state, params);
+    }
+
+    if (node.what !== undefined) {
+        walk(node.what, state, params);
+    }
+
+    if (node.source !== undefined) {
+        walk(node.source, state, params);
+    }
+
+    if (node.body !== undefined) {
+        // copy state to pass it to inner scope
+        // because inner scope shouldn't change outer scope
+        var innerState: State = {
+            vars: state.vars.slice(), // create new array
+            level: state.level
+        };
+        walk(node.body, innerState, params);
+    }
+}
+
 function parseToAST(code: string): any {
     var parser = new PhpParser({
         parser: {
@@ -21,17 +95,34 @@ function parseToAST(code: string): any {
     return ast;
 }
 
-function extractParams(ast: any): Array<any> {
-    return [];
+function obtainParams(ast: any): string[] {
+    var params: string[] = [];
+    var initState: State = {
+        vars: [],
+        level: 0
+    }
+    walk(ast, initState, params);
+    return params;
+}
+
+function toVariableNode(name: string): any {
+    return {
+        kind: "variable",
+        name: name
+    };
 }
 
 function wrapByFunction(ast: any): any {
-    const params = extractParams(ast);
+    const params = obtainParams(ast);
+    const paramNodes = params.map(param => {
+        return toVariableNode(param);
+    });
+    console.log(paramNodes);
 
     const funcWrapAST = [{
         kind: "function",
         name: "refactorFunc",
-        arguments: params,
+        arguments: paramNodes,
         body: {
             kind: "block",
             children: ast.children
