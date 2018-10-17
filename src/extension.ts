f689ae60af580c2f9859c40934482da096b94886
Tin Trinh <trinh.tin@hamee.co.jp>
2018-10-17 11:50:43 +0900

    add basic function

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,59 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+const PhpParser = require('php-parser');
+const UnParser = require('php-unparser');
+
+function parseToAST(code: string): any {
+    var parser = new PhpParser({
+        parser: {
+            extractDoc: true,
+            php7: true
+        },
+        ast: {
+            withPositions: true
+        }
+    });
+    var ast = parser.parseEval(code);
+    console.log(ast);
+
+    return ast;
+}
+
+function extractParams(ast: any): Array<any> {
+    return [];
+}
+
+function wrapByFunction(ast: any): any {
+    const params = extractParams(ast);
+
+    const funcWrapAST = [{
+        kind: "function",
+        name: "refactorFunc",
+        arguments: params,
+        body: {
+            kind: "block",
+            children: ast.children
+        }
+    }];
+    ast.children = funcWrapAST;
+
+    return ast;
+}
+
+function writeToStr(ast: any): string {
+    var options = {
+        indent: true,
+        dontUseWhitespaces: false,
+        shortArray: true,
+        bracketsNewLine: true,
+        forceNamespaceBrackets: false,
+        collapseEmptyLines: true
+    };
+
+    return UnParser(ast, options);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,16 +66,27 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    // let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-    //     // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('php.extractFunction', () => {
+        // The code you place here will be executed every time your command is executed
 
-    //     // Display a message box to the user
-    //     vscode.window.showInformationMessage('Hello World!');
-    // });
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage("There is no open window");
+            return; // No open text editor
+        }
 
-    // context.subscriptions.push(disposable);
+        let selection = editor.selection;
+        let text = editor.document.getText(selection);
+
+        editor.edit(builder => {
+            var ast = parseToAST(text);
+            ast = wrapByFunction(ast);
+            builder.replace(selection, writeToStr(ast));
+        });
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() { }
